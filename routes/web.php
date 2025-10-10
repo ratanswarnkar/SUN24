05a767e5c0041ef0a4fa7df22ead49446e922c04
ratanswarnkar <129288627+ratanswarnkar@users.noreply.github.com>
2025-10-10 16:39:07 +0530

    Update web.php
    product added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/about', [SolarController::class, 'about']);
 Route::get('/solar_for_home', [SolarController::class, 'saleForHome']);
 Route::get('solar_for_business', [SolarController::class, 'saleForBusiness']);
 Route::get('/contact_us', [SolarController::class, 'contactUs']);
+Route::get('/products', [SolarController::class, 'products']);
 
 
 
